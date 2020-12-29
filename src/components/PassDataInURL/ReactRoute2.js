69c0dbf585975da41c26b8f9777b081e5401b9ee
Malini Krishnamurthy <72509011+malinikrishnamurthy-14@users.noreply.github.com>
2020-12-29 19:26:41 +0530

    nested routes

--- src/components/PassDataInURL/ReactRoute2.js
+++ src/components/PassDataInURL/ReactRoute2.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Home from "./Home";
 import ProductsList from "./ProductsList";
+import NavigationBar from "./NavigationBar";
+import { Route, Switch } from "react-router-dom";
 
 const ReactRoute2 = () => {
   return (
     <div className="right-container">
-      <ProductsList />
+      <NavigationBar />
+      <div className="page-content">
+
+        <Switch>
+          <Route path="/PassDataInURL" component={Home} exact />
+          <Route path="/PassDataInURL/ProductsList" component={ProductsList} />
+        </Switch>
+        
+      </div>
+
     </div>
   );
 };
